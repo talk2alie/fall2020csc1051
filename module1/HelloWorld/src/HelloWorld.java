0d849fa032d655f14b80d1e8c642f318ea4779a1
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2020-07-22 22:36:05 -0400

    Add comment

--- module1/HelloWorld/src/HelloWorld.java
+++ module1/HelloWorld/src/HelloWorld.java
@@ -1,4 +1,7 @@
-
+/**
+ * This is a simple Java program that prints
+ * "Hello, world!" to the screen
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
