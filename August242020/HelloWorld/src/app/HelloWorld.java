ccaa6152a87f590b742d15235b0d4e063487e060
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2020-09-19 17:39:03 -0400

    Add quiz 1

---
+++ August242020/HelloWorld/src/app/HelloWorld.java
@@ -0,0 +1,16 @@
+package app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       
+        long bigNumber;
+        bigNumber = 124L;
+
+        int smallNumber;
+        smallNumber = 124;
+
+        double accountBalance = 55.66, endingBalance = 22_546.57;
+
+        System.out.println();
+    }
+}
---
+++ Quiz1/src/app/Questions.java
@@ -0,0 +1,19 @@
+//package app;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ * Questions
+ */
+public class Questions {
+    public static void main(String[] args) {
+        
+        File students = new File("Students.txt");
+
+        Scanner reader = new Scanner();
+
+
+        sys
+    }
+}
